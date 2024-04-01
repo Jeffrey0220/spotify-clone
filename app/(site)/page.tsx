9dc047ccb8549a5badc984aa83f9da8dbd05ce05
Jeffrey Li <jeff.a_@outlook.com>
2024-04-01 18:51:46 +1000

    create upload modal

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,8 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <div className="bg-neutral-900 h-full w-full rounded-lg overflow-hidden overflow-y-auto">
