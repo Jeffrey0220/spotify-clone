0530515f7a168179816b75ea3a826f3f33fcf106
Jeffrey Li <jeff.a_@outlook.com>
2024-04-02 22:35:13 +1000

    Load Image to SongItem

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["ahrsttfgjrcvsqcvmmvn.supabase.co"],
+  },
+};
 
 export default nextConfig;
