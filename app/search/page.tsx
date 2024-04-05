e67330a6a654101b6ac404ade66190f729ff37c2
Jeffrey Li <jeff.a_@outlook.com>
2024-04-05 23:26:26 +1000

    create SearchInput and SearchContent components

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,6 +2,7 @@ import getSongsByTitle from "@/actions/getSongsByTitle";
 import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
 import React from "react";
+import SearchContent from "./components/SearchContent";
 
 interface SearchProps {
   searchParams: {
@@ -12,11 +13,14 @@ interface SearchProps {
 const Search = async ({ searchParams }: SearchProps) => {
   const songs = await getSongsByTitle(searchParams.title);
   return (
-    <div>
+    <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header className="from-bg-neutral-900">
-        <h1 className="text-white text-3xl font-semibold">search</h1>
-        <SearchInput />
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl font-semibold">search</h1>
+          <SearchInput />
+        </div>
       </Header>
+      <SearchContent songs={songs} />
     </div>
   );
 };
